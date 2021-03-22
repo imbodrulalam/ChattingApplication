5af834b10b8dc4397a5ff010f290d05df15f403b
imbodrulalam <imbodrulalam@gmail.com>
2021-03-22 17:22:00 +0600

    Conversation layout updeted and attachment implementation started

--- app/src/main/java/com/example/chattingapplication/Activities/ChatActivity.java
+++ app/src/main/java/com/example/chattingapplication/Activities/ChatActivity.java
@@ -1,5 +1,6 @@
 package com.example.chattingapplication.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -112,6 +113,17 @@ public class ChatActivity extends AppCompatActivity {
 
                     }
                 });
+
+            }
+        });
+
+        binding.attachment.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/*");
+                startActivityForResult(intent, 25);
 
             }
         });
